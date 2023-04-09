fd38e6c59322f32a5095c56d77d81230d8911feb
Khan <armaan.khan@philips.com>
2023-04-09 13:35:30 +0530

    added Laser Firing Method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves up/down or right/left based on player input")]
     [SerializeField] float controlSpeed = 50f;
+    [Tooltip("How far ship moves right/left on screen")]
     [SerializeField] float xRange = 20f;
+    [Tooltip("How far ship moves up/down on screen")]
     [SerializeField] float yRange = 15f;
     float xThrow;
     float yThrow;
+
+    [Header("Screen Position based Tuning")]
     [SerializeField] float positionPitchFactor = -1.5f;
-    [SerializeField] float controlPitchFactor = -7.5f;
     [SerializeField] float positionYawFactor = 0.5f;
+
+    [Header("Player Input based Tuning")]
+    [SerializeField] float controlPitchFactor = -7.5f;
     [SerializeField] float controlRollFactor = -2f;
+
+    [Header("Laser Gun Array")]
+    [SerializeField] GameObject[] lasers;
 
     // Update is called once per frame
     void Update()
     {
         ProcessTranslation();
         ProcessRotation();
+        ProcessFiring();
+    }
+
+    void ProcessFiring()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SetLasersActive(true);
+        }
+        else
+        {
+            SetLasersActive(false);
+        }
+    }
+
+    void SetLasersActive(bool isActive)
+    {
+        foreach (GameObject laser in lasers)
+        {
+            var emissionModule = laser.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+        }
     }
 
     void ProcessRotation()
